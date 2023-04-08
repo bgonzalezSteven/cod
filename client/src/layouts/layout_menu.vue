eb98741575f4c1f2207a257fb7ac9064ac5bf3e3
Steven <bgonzales.wx@gmail.com>
2023-04-09 00:54:15 +0200

    TItulo en parte superior

--- client/src/layouts/layout_menu.vue
+++ client/src/layouts/layout_menu.vue
@@ -1,73 +1,77 @@
 <template>
   <q-layout view="hHh LpR lFf">
-    <q-page-container>
-      <q-header style="border-radius: 0em 0em 2em 2em" class="text-black">
-        <q-toolbar style="border-radius: 0em 0em 2em 2em" class="bg-primary text-white">
-          <q-btn dense flat round icon="short_text" @click="left = !left" />
-          <q-space />
-          <q-drawer show-if-above v-model="left" side="left" behavior="mobile">
-            <div class="row justify-center">
-              <q-img src="https://cdn.quasar.dev/img/blueish.jpg" style="height: 150px">
-                <div class="absolute-left" style="border-top-left-radius: 5px; background: none;">
-                  <q-avatar size="120px">
-                    <img src="https://cdn.quasar.dev/img/avatar4.jpg" />
-                  </q-avatar>
+    <q-header style="border-radius: 0em 0em 2em 2em" class="text-black">
+      <q-toolbar style="border-radius: 0em 0em 2em 2em" class="bg-primary text-white">
+        <q-btn dense flat round icon="short_text" @click="left = !left" />
+        <q-space />
+        <q-drawer show-if-above v-model="left" side="left" behavior="mobile">
+          <div class="row justify-center">
+            <q-img src="https://cdn.quasar.dev/img/blueish.jpg" style="height: 150px">
+              <div class="absolute-left" style="border-top-left-radius: 5px; background: none;">
+                <q-avatar size="120px">
+                  <img src="https://cdn.quasar.dev/img/avatar4.jpg" />
+                </q-avatar>
+              </div>
+              <div class="absolute-bottom-right text-bold " style="border-top-left-radius: 5px; background: none;">{{
+                state.info.name }}</div>
+            </q-img>
+          </div>
+          <div class="text-black q-pt-md" v-for="(item, index) in itemsMenu" v-bind:key="index">
+            <q-list>
+              <q-expansion-item group="somegroup" :icon="item.icon" :label="item.name" header-class="text-primary">
+                <div v-for="(itemChildren, indexChildren) in itemsMenu[index].children" v-bind:key="indexChildren">
+                  <q-list class="q-ml-lg">
+                    <q-item clickable v-ripple :to="itemChildren.url" v-if="login.can(itemChildren.permission)">
+                      <q-item-section avatar>
+                        <q-icon color="primary" :name="itemChildren.icon" />
+                      </q-item-section>
+
+                      <q-item-section>{{ itemChildren.name }}</q-item-section>
+                    </q-item>
+                  </q-list>
                 </div>
-                <div class="absolute-bottom-right text-bold " style="border-top-left-radius: 5px; background: none;">{{
-                  state.info.name }}</div>
-              </q-img>
-            </div>
-            <div class="text-black q-pt-md" v-for="(item, index) in itemsMenu" v-bind:key="index">
-              <q-list >
-                <q-expansion-item group="somegroup" :icon="item.icon" :label="item.name"
-                  header-class="text-primary">
-                  <div  v-for="(itemChildren, indexChildren) in itemsMenu[index].children" v-bind:key="indexChildren">
-                    <q-list class="q-ml-lg">
-                      <q-item clickable v-ripple :to="itemChildren.url" v-if="login.can(itemChildren.permission)">
-                        <q-item-section avatar>
-                          <q-icon color="primary" :name="itemChildren.icon" />
-                        </q-item-section>
+              </q-expansion-item>
+            </q-list>
+          </div>
+        </q-drawer>
 
-                        <q-item-section>{{itemChildren.name}}</q-item-section>
-                      </q-item>
-                    </q-list>
+        <q-btn flat round dense icon="account_circle">
+          <q-menu>
+            <div class="row no-wrap q-pa-md">
+              <div class="column items-center">
+                <q-avatar size="72px">
+                  <img src="https://cdn.quasar.dev/img/avatar4.jpg" />
+                </q-avatar>
+
+                <div class="text-weight-bold q-mt-md q-mb-xs">
+                  {{ state.info.name }}
+                </div>
+                <div class="text-weight-bold q-mt-md q-mb-xs">Usuario:</div>
+                <div class="text-weight">{{ state.info.email }}</div>
+                <div class="q-mt-md q-mb-xs"></div>
+                <div class="row">
+                  <div class="col">
+                    <q-btn color="primary" label="Cancel" push size="sm" v-close-popup />
                   </div>
-                </q-expansion-item>
-              </q-list>
-            </div>
-          </q-drawer>
-
-          <q-btn flat round dense icon="account_circle">
-            <q-menu>
-              <div class="row no-wrap q-pa-md">
-                <div class="column items-center">
-                  <q-avatar size="72px">
-                    <img src="https://cdn.quasar.dev/img/avatar4.jpg" />
-                  </q-avatar>
-
-                  <div class="text-weight-bold q-mt-md q-mb-xs">
-                    {{ state.info.name }}
-                  </div>
-                  <div class="text-weight-bold q-mt-md q-mb-xs">Usuario:</div>
-                  <div class="text-weight">{{ state.info.email }}</div>
-                  <div class="q-mt-md q-mb-xs"></div>
-                  <div class="row">
-                    <div class="col">
-                      <q-btn color="primary" label="Cancel" push size="sm" v-close-popup />
-                    </div>
-                    <div style="margin-left: 6%"></div>
-                    <div class="col">
-                      <q-btn color="negative" label="Logout" push size="sm" @click="exit()" />
-                    </div>
+                  <div style="margin-left: 6%"></div>
+                  <div class="col">
+                    <q-btn color="negative" label="Logout" push size="sm" @click="exit()" />
                   </div>
                 </div>
               </div>
-            </q-menu>
-          </q-btn>
+            </div>
+          </q-menu>
+        </q-btn>
 
-        </q-toolbar>
-      </q-header>
-        <router-view />
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <div class="q-pa-md">
+          <div class="text-bold text-h6" align="center">
+            {{ state.titulo }}
+          </div>
+      </div>
+      <router-view @setTittle="setTittle"/>
     </q-page-container>
   </q-layout>
 </template>
@@ -83,7 +87,8 @@ export default {
     const login = UseLoginInfo()
     const left = ref(false)
     let state = reactive({
-      info: {}
+      info: {},
+      titulo: 'Inicio'
     })
     const itemsMenu = [
       {
@@ -122,8 +127,11 @@ export default {
       login.logout()
       router.push('/')
     }
+    function setTittle(params) {
+      state.titulo = params
+    }
     return {
-      left, state, onMounted, exit, itemsMenu, login
+      left, state, onMounted, exit, itemsMenu, login, setTittle
     }
   },
 }
--- client/src/pages/admin/listDocuments.vue
+++ client/src/pages/admin/listDocuments.vue
@@ -1,3 +1,25 @@
 <template>
-  <div>QLQ2</div>
+  <q-page class="q-pa-sm" style="min-height: 420px;">
+    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+      <div class="q-pa-sm">
+        <div class="row items-center justify-center">
+          aja2
+        </div>
+      </div>
+    </div>
+  </q-page>
 </template>
+<script>
+import { onBeforeMount } from 'vue';
+export default {
+
+  setup(props, { emit }) {
+    onBeforeMount(() => {
+      emit('setTittle', 'Listado')
+    })
+    return {
+
+    }
+  }
+}
+</script>
